da239ed585147793fe197c3532c7c39f406ef29f
g22 <aap.kk@list.ru>
2021-04-15 10:54:17 +0300

    Hometask

---
+++ dz/5(OpenServer+IzmenenniySaitVizitka)/5/mysite/components/about.php
@@ -0,0 +1,9 @@
+<section class="about container">
+  <h2 class="section-title">Привет, я <?= $name ?></h2>
+  <img class="about-image" src="<?= $image ?>" width="147" height="147" alt="<?= $name ?>">
+  <div class="about-content">
+    <p>Я хочу войти в айти. </p>
+    <p>В действительности все выглядит иначе, чем на самом деле</p>
+    <p>Яблоки на балконе, а должны быть на снегу</p>
+  </div>
+</section>
---
+++ dz/5(OpenServer+IzmenenniySaitVizitka)/5/mysite/index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Как войти в айти?';
+$name = 'Linda';
+$image = 'img/unknown-raccoon.svg';
+$email = 'linda@gmail.com';
+$phone = '+88005553535';
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
